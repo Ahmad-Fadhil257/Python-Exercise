ffdc76bb862a432491f918164bb82974dc163f95
Ahmad-Fadhil257 <2zi009adf@gmail.com>
2024-10-01 11:12:29 +0700

    Mencari Volume Kubus

---
+++ kubus.py
@@ -0,0 +1,11 @@
+print("        VOLUME KUBUS         ")
+
+Panjang = int(input("Masukkan Panjang (cm)  : "))
+tinggi = int(input("Masukkan Tinggi (cm)    : "))
+lebar = int(input("Masukkan Lebar (cm)      : "))
+
+Kubus = Panjang * tinggi * lebar
+m = Kubus / 100
+
+print (f"VOLUME KUBUS ADALAH: {Kubus} cm ")
+print (f"VOLUME KUBUS ADALAH: {m} m ")
